b85f9b740901c3c9840fdb8ac2d2a2a876daa1df
Alice <oi17alm@cs.umu.se>
2021-05-05 10:34:25 +0200

    Interest init commit

--- app/src/main/java/se/umu/yarn/InterestsActivity.java
+++ app/src/main/java/se/umu/yarn/InterestsActivity.java
@@ -3,8 +3,12 @@ package se.umu.yarn;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static android.util.Log.d;
 
@@ -15,16 +19,35 @@ class InterestsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         d("Alice: ", "Hello");
         setContentView(R.layout.interests);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+
 
         final Button confirmButton = findViewById(R.id.confirmButton);
 
+        final RadioButton radioButton = findViewById(R.id.radioButton2);
+
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
+                d("Alice", "Clicked");
             }
         });
 
-    }
 
 
-}
+        radioButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                d("Alice", "radioButton");
+
+                boolean checked = ((RadioButton) radioButton).isChecked();
+
+                // Check which radio button was clicked
+               radioButton.setChecked(!checked);
+            }
+
+
+        });
+    }}
