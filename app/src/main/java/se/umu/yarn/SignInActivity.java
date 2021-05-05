c5c2ddbda94f739549c42aed3bd51add96f82b64
Robin Forsström <robin.forsstrom1@gmail.com>
2021-05-05 10:25:05 +0200

    Added SignInActivity, signInLayout,

---
+++ app/src/main/java/se/umu/yarn/SignInActivity.java
@@ -0,0 +1,115 @@
+package se.umu.yarn;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
+
+    GoogleSignInClient mGoogleSignInClient;
+    //RC_SIGN_IN
+    SignInButton signInButton;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_login_oauth);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        //signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("onStart", "Inne i onStart");
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //updateUI(account);
+
+        // Update your UI accordingly—that is, hide the sign-in button,
+        // launch your main activity, or whatever is appropriate for your app.
+
+
+    }
+
+    @Override
+    public void onClick(View v)  {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        //RC_SIGN_IN
+        Log.d("SignIn", "Inne i sign in ");
+
+        startActivityForResult(signInIntent, 15);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("Activity resukt", "Inne i activity result: " + requestCode );
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 15) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+
+        Log.d("handle", "Inne i handle: " );
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            Log.d("myTag",account.getEmail() );
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+
+
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w( "fail", "nånting gick fel ");
+            //updateUI(null);
+        }
+    }
+
+
+}
